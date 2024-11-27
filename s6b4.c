71b71fd4c1676ccb40573c5db0ed6af8de5baf6f
duydoan169 <hanh250470@gmail.com>
2024-11-27 08:20:59 +0700

    Update s6b4.c

--- s6b4.c
+++ s6b4.c
@@ -7,13 +7,13 @@ int main(){
     printf("nhap vao 3 so a b c: ");
     scanf("%d %d %d", &a, &b, &c);
         delta=b*b - 4*a*c;
-    }while(a == 0 || delta < 0 );
+    }while(a == 0);
     if(delta<0)
         printf("phuong trinh vo nghiem");
     if(delta==0)
         printf("phuong trinh co nghiem kep x1 = x2 = %d", -b/(2*a));
     if(delta>0)
-        printf("phuong trinh co 2 nghiem phan biet x1 = %.1f, x2 = %.1f", (-b+sqrt((float)delta))/(2*a), (-b-sqrt((float)delta))/(2*a));
+        printf("phuong trinh co 2 nghiem phan biet x1 = %.1f, x2 = %.1f", (-b+sqrt(delta))/(2*a), (-b-sqrt(delta))/(2*a));
     
     return 0; 
 }
